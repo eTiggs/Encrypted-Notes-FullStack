e5016eecfb7057071499c9ccc2eb2d3003f41db7
eTiggs <63173961+eTiggs@users.noreply.github.com>
2024-06-27 14:01:03 +0100

    FIX: Simplified and fixed config.js

--- NoteAppService/src/config/config.js
+++ NoteAppService/src/config/config.js
@@ -1,12 +1,11 @@
-import dotenv from 'dotenv';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import dotenv from "dotenv";
 
 export default class Config {
-    static load() {
-        const envFile = process.env.NODE_ENV === 'prod' ? '.env' : '.env.dev';
-        dotenv.config({
-            path: join(dirname(fileURLToPath(import.meta.url)), `../../${envFile}`),
-        });
-    }
+  static #env = process.env.NODE_ENV;
+
+  static load = () => {
+    dotenv.config({
+      path: `.env${Config.#env !== "prod" ? `.${Config.#env}` : " "}`,
+    });
+  };
 }
