22ec506e62b9e7f8069d1d680c073e0014bd82fb
eTiggs <63173961+eTiggs@users.noreply.github.com>
2024-07-02 15:55:26 +0100

    HOTFIX: test

--- NoteAppService/src/controllers/noteController.js
+++ NoteAppService/src/controllers/noteController.js
@@ -32,7 +32,7 @@ export const getNote = async (req, res) => {
   try {
     const note = await Note.findById(id);
     if (!note) {
-      return res.status(404).json({ message: 'Note not found' });
+      return res.status(404).json({ message: 'Notee not found' });
     }
 
     if (note.userId !== userId) {
