f7c3c4bcdc6a92fb784ba5ff3e1c39285d9dbb1f
eTiggs <63173961+eTiggs@users.noreply.github.com>
2024-07-02 11:19:23 +0100

    HOTFIX: Name update

--- NoteAppService/src/routes/authRouter.js
+++ NoteAppService/src/routes/authRouter.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import AuthController from '../controllers/AuthController.js';
+import AuthController from '../controllers/authController.js';
 import AuthValidator from '../middleware/authValidator.js';
 
 export default class AuthRouter {
