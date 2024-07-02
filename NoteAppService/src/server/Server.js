2a266134877ce0dc18c51d831eb7b465ea8cfe69
eTiggs <63173961+eTiggs@users.noreply.github.com>
2024-07-02 16:59:49 +0100

    HOTFIX: broke

--- NoteAppService/src/server/Server.js
+++ NoteAppService/src/server/Server.js
@@ -21,7 +21,7 @@ export default class Server {
 
     #configureMiddleware() {
         const corsOptions = {
-            origin: 'https://encrypted-notes-fullstack.onrender.com',
+            origin: 'https://encrypted-notes-fullstack-1.onrender.com',
             methods: ['GET', 'POST', 'PUT', 'DELETE'],
             allowedHeaders: ['Content-Type', 'Authorization'],
             credentials: true,
